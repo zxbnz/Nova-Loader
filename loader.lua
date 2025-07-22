2da56f6941122d0478774d2eb62829795be0c631
zxbnz <159658374+zxbnz@users.noreply.github.com>
2025-07-22 12:59:25 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -65,11 +65,13 @@ local function makeButton(text, positionY, url)
 	end)
 end
 
+
 -- Buttons
 makeButton("ValoBlox", 40, "https://raw.githubusercontent.com/zxbnz/Valolo-Blox/refs/heads/main/code.lua")
 makeButton("Jailbird", 80, "https://raw.githubusercontent.com/zxbnz/JailBird-Cheat/refs/heads/main/code.lua")
 makeButton("Operation Siege", 120, "https://raw.githubusercontent.com/zxbnz/OS-Cheat/refs/heads/main/code.lua")
-makeButton("CounterBlox", 160, "https://raw.githubusercontent.com/zxbnz/CounterBlox/refs/heads/main/script.lua")
+makeButton("Counter Blox", 160, "https://raw.githubusercontent.com/zxbnz/CounterBlox-Script/main/code.lua")
+
 
 -- Dragging
 local dragging = false
